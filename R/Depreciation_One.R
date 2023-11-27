2d436fcfcc4e2f4bfad4b684a9fbdb4dbc0a1462
HarunCelikOtto <hcelik@iastate.edu>
2023-11-27 08:57:15 -0600

    Start documentation of depreciation one functions

---
+++ R/Depreciation_One.R
@@ -0,0 +1,41 @@
+#' Returns Depreciation Values for Scenario One Costs
+#'
+#' @description
+#' This function returns a list of depreciation values for costs related to the building being
+#' owned by the business operating the store. The returned values include annual depreciation values
+#' for Building or Remodeling, Parking Lot Improvements, Shelving and Check Out Counters, Computer Equipment and POS
+#' Vehicles, Display Cases (frozen or refrigerated), Refrigeration, Freezers, Meat Cutting Equipment,
+#' and the Amortization of Start-Up Costs
+#'
+#' @param build_life life years for building/remodeling
+#' @param parklot_life life years for parking lot improvements
+#' @param shelve_life life years for shelving and check out counters
+#' @param computer_life life years for computer equipment and POS
+#' @param vehicles life years for vehicles
+#' @param displaycase_life life years for display cases
+#' @param refridge_life life years for refrigeration
+#' @param freezer_life life years for freezers
+#' @param meatcut_life life years for meat cutting equipment
+#' @param startup_life life years for amortization of start-up costs
+#' @param assetone_life life years for miscellaneous asset one
+#' @param assettwo_life life years for miscellaneous asset two
+#' @param assetthree_life life years for miscellaneous asset three
+#'
+#' @param build_cost cost of building/remodeling
+#' @param parklot_cost cost of parking lot improvements
+#' @param shelve_cost cost of shelving and check out counters
+#' @param computer_cost cost of computer equipment and POS
+#' @param vehicost cost of vehicles
+#' @param displaycase_cost cost of display cases
+#' @param refridge_cost cost of refrigeration
+#' @param freezer_cost cost of freezers
+#' @param meatcut_cost cost of meat cutting equipment
+#' @param startup_cost cost of amortization of start-up costs
+#' @param assetone_cost cost of miscellaneous asset one
+#' @param assettwo_cost cost of miscellaneous asset two
+#' @param assetthree_cost cost of miscellaneous asset three
+#'
+#' @author Harun Celik
+
+Depreciation_One <- function() {}
+
