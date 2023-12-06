ee6c17dc49f9dcafed6247249f8598b8aa99fff4
HarunCelikOtto <hcelik@iastate.edu>
2023-12-06 17:09:26 -0600

    Update calculation to include a town in county AND in market area

--- R/Market_Populations.R
+++ R/Market_Populations.R
@@ -14,12 +14,14 @@
 #' @author Harun Celik
 #' @export
 
-Market_Populations <-  function(county_pct, metro_pop, county_pop, county_cities, non_county_cities) {
+Market_Populations <-  function(county_pct, metro_pop, county_pop, county_cities, county_market_cities, non_county_cities) {
 
   # Sum the list of county and non-county cities #
-
+  browser()
   ## Take the sum of all the county cities
   county_cities_pop <- Reduce("+", county_cities)
+  ## Take the sum of all the in county AND in market cities
+  county_market_cities_pop <- Reduce("+", county_market_cities)
   ## Take the sum of all non-county cities
   non_county_cities_pop <- Reduce("+", non_county_cities)
 
@@ -27,20 +29,23 @@ Market_Populations <-  function(county_pct, metro_pop, county_pop, county_cities
   # Calculating the rural population for the market. #
 
   ## Calculate total rural population
-  total_town_pop <- (metro_pop + county_cities_pop + non_county_cities_pop)
+  total_town_pop <- (metro_pop + county_cities_pop + county_market_cities_pop)
   total_rural_pop <- (county_pop - total_town_pop)
-  ## Multiply the county percentage with the total rural population
+  ## Multiply the market county percentage with the total rural population
   market_rural_pop <- floor(total_rural_pop * county_pct)
 
+  ## Calculating all populations in the market area
+  total_market_area_pop <- (non_county_cities_pop + county_market_cities_pop)
+
   # Calculating the town population (metro + market rural pop + non county cities)
-  total_market_pop <- (metro_pop + market_rural_pop + non_county_cities_pop)
+  total_market_pop <- (metro_pop + market_rural_pop + total_market_area_pop)
 
   # Return List
-  Populations_List <- list(metro_pop = metro_pop,
-                           rural_market_pop = market_rural_pop,
-                           non_county_cities_pop = non_county_cities_pop,
-                           total_market_pop = total_market_pop,
-                           total_town_pop = total_town_pop)
+  Populations_List <- list(metro_pop = metro_pop, # City of Store
+                           rural_market_pop = market_rural_pop, # Total Rural Population in Market
+                           market_cities_pop = total_market_area_pop, # Total-Towns in Market
+                           total_market_pop = total_market_pop, # Total Market Size
+                           total_town_pop = total_town_pop) # All towns within county (including a portion of the market size)
 
   return(Populations_List)
 }
