55e6e17a7c4c5a7ab825ea75cfb8d64bfd5e71bd
karthik-H <karthikhanumanthaiah@gmail.com>
2023-12-25 20:19:42 -0600

    fix: rewrite Interest

--- R/Interest.R
+++ R/Interest.R
@@ -10,13 +10,13 @@
 
 Interest <- function(interest_table) {
 
-  interest_table$interest <- (interest_table$interest / 100)
+  interest_table$interest_pct <- (interest_table$interest / 100)
 
-  interest_table$calculated_interest <- (interest_table$loan * interest_table$interest)
+  interest_table$calculated_interest <- (interest_table$loan * interest_table$interest_pct)
 
   total_interest <- sum(interest_table$calculated_interest, interest_table$loan, na.rm = T)
 
-  Interest_List <- list(interest_table = interest_table,
+  Interest_List <- list(interest_table = subset(interest_table, select = -c(interest_pct) ),
                         total_interest = total_interest)
 
 }
