11104f2cc329d0810065ab058f82c1c992594f27
HarunCelikOtto <hcelik@iastate.edu>
2024-01-15 14:05:18 -0600

    Update interest calculation to sum only the interest amounts.

--- R/Interest.R
+++ R/Interest.R
@@ -14,7 +14,7 @@ Interest <- function(interest_table) {
 
   interest_table$calculated_interest <- (interest_table$loan * interest_table$interest_pct)
 
-  total_interest <- sum(interest_table$calculated_interest, interest_table$loan, na.rm = T)
+  total_interest <- sum(interest_table$calculated_interest, na.rm = T)
 
   Interest_List <- list(interest_table = subset(interest_table, select = -c(interest_pct) ),
                         total_interest = total_interest)
